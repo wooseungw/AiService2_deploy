767f186853249b778c8cd95f65e0c85ed557078f
parkwoobin <oi3oi3oi@naver.com>
2024-12-11 00:06:15 +0900

    Update weather_func.py

--- weather_func.py
+++ weather_func.py
@@ -2,11 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
-}
-response = requests.get(url, headers=headers)
-
 
 mapping = {
     'Seoul': '서울',
